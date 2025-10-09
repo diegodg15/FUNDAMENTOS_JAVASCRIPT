e002178192c90e7d566a374e0f5d95b6326cadcc
diego <diego.guerrero5dg@outlook.com>
2025-10-09 12:39:52 -0300

    Agregar tipos de datos primitivos JS.

---
+++ tipos-datos-js/datos-primitivos.js
@@ -0,0 +1,15 @@
+
+//TIPOS DE DATOS PRIMITIVOS JS
+
+/**Listado de datos primitivos:
+ * 1. STRING
+ * 2. NUMBER
+ * 3. BOOLEAN
+ * 4. NULL
+ * 5. UNDEFINED
+ * 6. BIGINT
+ * 7. SYMBOL
+ */
+//-----------------------------------------------------
+
+// STRING:
